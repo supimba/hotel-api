1a6d62e65a7e88554ef6e18d26d2769fc89d007c
Tanner Goins <tannergz28@gmail.com>
2017-09-22 20:09:56 -0500

    initial commit

---
+++ HotelApi/Factories/MyDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using PostgresEFCore.Providers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostgresEFCore.Factories
+{
+    public class MyDbContextFactory : IDesignTimeDbContextFactory<Context>
+    {
+        public Context CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<Context>();
+
+            builder.UseNpgsql("Host=localhost;Username=postgres;Password=password;Database=HotelManagement");
+            return new Context(builder.Options);
+        }
+    }
+}
---
+++ HotelApi/Providers/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostgresEFCore.Providers
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Car>(entity => {
+        }
+
+        //public virtual DbSet<Car> Cars { get; set; }
+    }
+}
