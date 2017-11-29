adb5248aa77829e185d60d3bf145ca37c8de3939
Jerry Vikrathe <services.box@outlook.com>
2017-11-28 21:36:04 -0500

    Re-organized and re-wrote large sections of Context.cs.

--- HotelApi/DbContext/Context.cs
+++ HotelApi/DbContext/Context.cs
@@ -1,17 +1,29 @@
-﻿/* SETTING UP THE DATABASE FOR THE HOTEL RESERVATION SYSTEM
+﻿/* SETTING UP THE DATABASE FOR THE HOTEL RESERVATION SYSTEM PROJECT
  *
+ * 
  * This project will store data in a PostgreSQL database and it will use Entity Framework Core for
- * the ORM. EF Core acts as a bridge between the Hotel Reservation project and the database,
- * translating C# code into SQL commands and C# objects into records in the database, without
- * writing SQL.
+ * the ORM. EF Core acts as a translation layer between the Hotel Reservation project and the 
+ * database, translating C# code into SQL commands and C# objects into records in the database, all 
+ * without writing SQL.
  *
- * To facilitate communications between the program and a database, EF Core needs to know the:
+ *                    PROGRAM <----> ENTITY FRAMEWORK CORE <---> DATABASE
+ *
+ * If EF Core is to perform these translations, it must understand the topology of the database. To
+ * that end, EF Core needs to know the following:
  * 
- *      1) Data Provider and, (defined in Factories/MyDbContextFactory.cs)
- *      2) Data Model (defined in DbContext/Context.cs)
- *      3) Database Schema (Generated in the Migrations Folder)
+ *      1) Data Provider,              (Defined in Factories/MyDbContextFactory.cs)
+ *      2) Data Model,                 (Defined in DbContext/Context.cs)
+ *      3) Database Schema             (Generated in the Migrations Folder)
  * 
- * This file defines a Data Model for the database.
+ * The Data Provider is a small library that helps EF Core to talk to databases from a given vendor.
+ * For instance, there are Data Providers for SQL Server, SQLite, PostgreSQL and other databases.
+ * The general layout of the database and its entities are defined in the Data Model. The DM is
+ * contained in this file. The Schema is the more specific database layout. It is a map to the 
+ * current topology of the database and it can be generated on command. As development on a database 
+ * continues, it gains or loses tables and other entities over time. In other words, the topology of
+ * the database is changing. Schemas serve as a snapshot of the topology of a database at certain
+ * point in time. Thus, schemas are used to migrate data across versions of the database and hence
+ * are called migrations. Unsurprisingly, you can find them in the Migrations folder.
  * 
  * 
  * DATA PROVIDERS
@@ -39,13 +51,33 @@
  * 
  * In order to do two-way translations, the ORM library (EF Core) needs a Data Model of the
  * database. A Data Model is an abstract formalization of the entities in a database and their
- * relationships. It conceptually standardizes how entites relate to one another. In this file,
- * this is the Data Model for a Hotel table:
+ * relationships. It conceptually standardizes how entites relate to one another. The Data Model
+ * is distinct from a Database Schema; see this StackOverflow Answer from richik jaiswal:
+ * 
+ *      A schema is a blueprint of the database which specifies what fields will be present and 
+ *      what their types will be. For example an employee table will have an employee_ID column 
+ *      represented by a string of 10 digits and an employee_Name column with a string of 45 
+ *      characters.
+ *      
+ *      Data model is a high level design implementation which decides what can be present in the 
+ *      schema. It provides a database user with a conceptual framework in which we specify the 
+ *      database requirements of the database user and the structure of the database to fulfill 
+ *      these requirements.
+ *      
+ *      A data model can, for example, be a relational model where the data will be organised in 
+ *      tables whereas the schema for this model would be the set of attributes and their 
+ *      corresponding domains.
+ *      
+ *      (Source: https://stackoverflow.com/questions/25093452/difference-between-data-model-and-database-schema-in-dbms)
+ *
+ * Therefore, a Data Model is a general blueprint that defines some properties of the Schema. Taken
+ * from this file, here is the Data Model for a Hotel table:
  *
  *
  *     public class Context : DbContext
  *     {
  *         public virtual DbSet<Hotel> Hotels { get; set; }
+ * 
  *         protected override void OnModelCreating(ModelBuilder modelBuilder)
  *         {
  *             modelBuilder.Entity<Hotel>(entity =>
@@ -62,16 +94,20 @@
  * A data structure is defined to hold the properties of the Hotel table, followed by definitions of 
  * these properties. HasKey() denotes the Id field as the Primary Key of a table. The other 
  * properties are defined with the Property() method. The Id, Name, Address and PhoneNumber fields
- * are marked as required fields. The character lengths of each field is also specified. This
- * defines a general, high-level set of constraints on Hotel tables in this database. 
+ * are marked as required fields. The character lengths of each field is also specified. A reminder
+ * that this is the Data Model for only the Hotel table. It specifies general properties of the 
+ * columns in the Hotel Table. The purpose of a Data Model is to broadly define the shape and form 
+ * of the entities in a database in a high-level fashion. 
  * 
- * The Data Model is conceptual framework that shapes and decides the contents of the Schema. A
- * Database Schema is a more concrete set of rules that govern the database. There are schemas for 
- * at least three levels of a database: physical schema (how data blocks are stored at the lowest
- * level; this is where database designers work), logical schema (how records get stored in data
- * structures; this level is where programmers and DB administrators work) and view schemas (which
- * are schemas for end-user interaction). Data Models are not intended to be easily changed, but
- * Schemas are.
+ * The Data Model is a conceptual framework that constrains the contents of the Schema. .NET Core
+ * and EF Core probably combine the type information from the Model classes with the Data Model
+ * defined in this file to generate the SCHEMA. A Database Schema is a more concrete set of rules
+ * that govern the database. Data Models are not intended to be easily changed, but Schemas are.
+ *
+ * There are schemas for at least three levels of a database: physical schema (how data blocks are
+ * stored at the lowest level; this is where database designers work), logical schema (how records
+ * get stored in data structures; this level is where programmers and DB administrators work) and
+ * view schemas (which are schemas for end-user interaction). 
  *
  * This is the Logical schema for the Hotel table: 
  *
@@ -108,33 +144,6 @@
  * assigns types to each column, and other properties such as nullability and annotations. Note that
  * the character length of the columns are inherited from the Data Model.
  *
- * 
- * The Data Model is distinct from a Database Schema; see this StackOverflow Answer from 
- * richik jaiswal:
- * 
- *      A schema is a blueprint of the database which specifies what fields will be present and 
- *      what their types will be. For example an employee table will have an employee_ID column 
- *      represented by a string of 10 digits and an employee_Name column with a string of 45 
- *      characters.
- *      
- *      Data model is a high level design implementation which decides what can be present in the 
- *      schema. It provides a database user with a conceptual framework in which we specify the 
- *      database requirements of the database user and the structure of the database to fulfill 
- *      these requirements.
- *      
- *      A data model can, for example, be a relational model where the data will be organised in 
- *      tables whereas the schema for this model would be the set of attributes and their 
- *      corresponding domains.
- *      
- *      (Source: https://stackoverflow.com/questions/25093452/difference-between-data-model-and-database-schema-in-dbms)
- *
- * Therefore, a Data Model is a general blueprint that defines some properties of the Schema. 
- *
- * 
- * Context.cs serves as a bridge between the model classes (classes that model database tables in 
- * the Models/Tables folder in the Common project) and database entities (records stored in the 
- * database).
- *
  *
  ***************************************************************************************************
  ***************************************************************************************************
@@ -142,15 +151,27 @@
  * 
  * CONTEXT CLASS
  *
+ * Recall that EF Core will attempt to do two-way translations between this program and the
+ * database.
+ * 
+ *                    PROGRAM <----> ENTITY FRAMEWORK CORE <---> DATABASE
+ * 
+ * This file contains one of the three things EF Core requires, namely, the Data Model.
+ * 
  * This class, Context, inherits from an EF Core class called Microsoft.EntityFrameworkCore.DbContext.
  * From the docs: "A DbContext instance represents a session with the database and can be used to
- * query and save instances of your entities." Put succintly, DbContext helps you talk to the
- * database. If EF Core is to act as a bridge, it must know the topology of the database. The
- * programmer must create Context.cs and set it to inherit from DbContext. Then, the programmer has
- * to outline certain information about the database in Context class.
+ * query and save instances of your entities." Put succintly, the DbContext class helps you talk to
+ * the database.
  *
- * In this class, the programmer must use the Models classes (from project Common) to define the
- * Data Model of the database. 
+ * In Context class, the programmer must specify the general parameters for Tables and other
+ * entities in the database. As such, it serves as a bridge between the model classes (classes that
+ * model database tables in the Models/Tables folder in the Common project) and database entities
+ * (records stored in the database).
+ *
+ * The programmer must create Context.cs and set it to inherit from DbContext. Then, the programmer
+ * has to outline certain information about the database in this class. Use information from the
+ * Model classes (from project Common) to define the Data Model of the database. See below for more
+ * details on how to do this.
  */
 
 
