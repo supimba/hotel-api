4217813ab35c6c658574ccc616eaa2b3589bcdf0
Jerry Vikrathe <services.box@outlook.com>
2017-11-28 19:57:23 -0500

    Three new packages added to test if Controllers can be generated via dotnet Core CLI.

--- HotelApi/Factories/MyDbContextFactory.cs
+++ HotelApi/Factories/MyDbContextFactory.cs
@@ -16,6 +16,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace PostgresEFCore.Factories
 {
     public class MyDbContextFactory : IDesignTimeDbContextFactory<Context>
