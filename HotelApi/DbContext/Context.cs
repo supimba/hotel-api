3fd8af7726903b522a21ff69e8cfe6eac3009dc3
Jerry Vikrathe <services.box@outlook.com>
2017-11-28 19:55:59 -0500

    Context.cs comments were heavily edited; a section on Database Data Models & Database Schema was added.

--- HotelApi/DbContext/Context.cs
+++ HotelApi/DbContext/Context.cs
@@ -1,14 +1,18 @@
-﻿/* SETTING UP COMMUNICATIONS BETWEEN THE PROGRAM AND THE DATABASE
+﻿/* SETTING UP THE DATABASE FOR THE HOTEL RESERVATION SYSTEM
  *
- * This project uses PostgreSQL for the database and Entity Framework Core for the ORM. EF Core
- * acts as a bridge between the program and the database, translating C# code into SQL commands
- * and C# objects into records in the database.
+ * This project will store data in a PostgreSQL database and it will use Entity Framework Core for
+ * the ORM. EF Core acts as a bridge between the Hotel Reservation project and the database,
+ * translating C# code into SQL commands and C# objects into records in the database, without
+ * writing SQL.
  *
  * To facilitate communications between the program and a database, EF Core needs to know the:
  * 
- *      1) Data Provider and,
- *      2) Data Model (which is a Map to the layout of the entire Database).
- *
+ *      1) Data Provider and, (defined in Factories/MyDbContextFactory.cs)
+ *      2) Data Model (defined in DbContext/Context.cs)
+ *      3) Database Schema (Generated in the Migrations Folder)
+ * 
+ * This file defines a Data Model for the database.
+ * 
  * 
  * DATA PROVIDERS
  *  
@@ -23,7 +27,7 @@
  * When HotelApi runs, Entity Framework will search for a Data Provider. EF Core will use the Data
  * Provider to determine that this project uses a PostgreSQL database. 
  *  
- * EF Core will use a Data Model Map to transform a SQL query into an object model representation
+ * EF Core will use the Data Model to transform a SQL query into an object model representation
  * of a query (which is called a canonical command tree) and vice versa. .NET Data Providers can 
  * consume canonical command trees to talk the database.
  *
@@ -31,12 +35,79 @@
  * in that file.
  * 
  * 
- * DATA MODEL
+ * DATABASE DATA MODEL vs. DATABASE SCHEMA
  * 
- * In order to do two-way translations, the ORM library (EF Core) needs to have a map of the 
- * database. The Data Model is a topological map of the whole database. The Data Model is a
- * list of all the tables in the database (Models folder) and their relationships. In this 
- * project, the Data Model is defined in a file called Context.cs in the DbContext folder. 
+ * In order to do two-way translations, the ORM library (EF Core) needs a Data Model of the
+ * database. A Data Model is an abstract formalization of the entities in a database and their
+ * relationships. It conceptually standardizes how entites relate to one another. In this file,
+ * this is the Data Model for a Hotel table:
+ *
+ *
+ *     public class Context : DbContext
+ *     {
+ *         public virtual DbSet<Hotel> Hotels { get; set; }
+ *         protected override void OnModelCreating(ModelBuilder modelBuilder)
+ *         {
+ *             modelBuilder.Entity<Hotel>(entity =>
+ *             {
+ *                 entity.HasKey(e => e.Id);
+ *                 entity.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+ *                 entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+ *                 entity.Property(e => e.Address).IsRequired().HasMaxLength(200);
+ *                 entity.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(100);
+ *             });
+ *         }
+ *     }
+ *         
+ * A data structure is defined to hold the properties of the Hotel table, followed by definitions of 
+ * these properties. HasKey() denotes the Id field as the Primary Key of a table. The other 
+ * properties are defined with the Property() method. The Id, Name, Address and PhoneNumber fields
+ * are marked as required fields. The character lengths of each field is also specified. This
+ * defines a general, high-level set of constraints on Hotel tables in this database. 
+ * 
+ * The Data Model is conceptual framework that shapes and decides the contents of the Schema. A
+ * Database Schema is a more concrete set of rules that govern the database. There are schemas for 
+ * at least three levels of a database: physical schema (how data blocks are stored at the lowest
+ * level; this is where database designers work), logical schema (how records get stored in data
+ * structures; this level is where programmers and DB administrators work) and view schemas (which
+ * are schemas for end-user interaction). Data Models are not intended to be easily changed, but
+ * Schemas are.
+ *
+ * This is the Logical schema for the Hotel table: 
+ *
+ *     public partial class CreateHotelHotelRoomRoomTypeBedTypeRoomReservationTables : Migration
+ *     {
+ *         protected override void Up(MigrationBuilder migrationBuilder)
+ *         {
+ *             migrationBuilder.CreateTable(
+ *                 name: "Hotels",
+ *                 columns: table => new
+ *                 {
+ *                     Id = table.Column<long>(type: "int8", nullable: false)
+ *                         .Annotation("Npgsql:ValueGenerationStrategy",
+ *                                      NpgsqlValueGenerationStrategy.SerialColumn),
+ *                     Address = table.Column<string>(type: "varchar(200)",
+ *                                                    maxLength: 200,
+ *                                                    nullable: false),
+ *                     Name = table.Column<string>(type: "varchar(100)",
+ *                                                 maxLength: 100,
+ *                                                 nullable: false),
+ *                     PhoneNumber = table.Column<string>(type: "varchar(100)",
+ *                                                        maxLength: 100,
+ *                                                        nullable: false)
+ *                 },
+ *                 constraints: table =>
+ *                 {
+ *                     table.PrimaryKey("PK_Hotels", x => x.Id);
+ *                 });
+ *         }
+ *     }
+ *
+ * The Hotel entity defined the Data Model is explicitly defined as a Hotels Table in the schema.
+ * This table's columns are given names defined in the Data Model. The schema goes further and
+ * assigns types to each column, and other properties such as nullability and annotations. Note that
+ * the character length of the columns are inherited from the Data Model.
+ *
  * 
  * The Data Model is distinct from a Database Schema; see this StackOverflow Answer from 
  * richik jaiswal:
@@ -57,6 +128,9 @@
  *      
  *      (Source: https://stackoverflow.com/questions/25093452/difference-between-data-model-and-database-schema-in-dbms)
  *
+ * Therefore, a Data Model is a general blueprint that defines some properties of the Schema. 
+ *
+ * 
  * Context.cs serves as a bridge between the model classes (classes that model database tables in 
  * the Models/Tables folder in the Common project) and database entities (records stored in the 
  * database).
@@ -71,7 +145,7 @@
  * This class, Context, inherits from an EF Core class called Microsoft.EntityFrameworkCore.DbContext.
  * From the docs: "A DbContext instance represents a session with the database and can be used to
  * query and save instances of your entities." Put succintly, DbContext helps you talk to the
- * database. To enable EF Core to act as a bridge, it must know the topology of the database. The
+ * database. If EF Core is to act as a bridge, it must know the topology of the database. The
  * programmer must create Context.cs and set it to inherit from DbContext. Then, the programmer has
  * to outline certain information about the database in Context class.
  *
